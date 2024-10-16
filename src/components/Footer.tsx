35a9a31f49f11341805fcfe94d3506909fef55ad
Amaru Ríos <128440111+Am4rv@users.noreply.github.com>
2024-10-16 15:32:39 -0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer: React.FC = () => {
         <footer className={styles.footer}>
             <p>Pokédex App © 2024</p>
             <p>
-                Creado por <a href="hhttps://github.com/Am4rv" target="_blank" rel="noopener noreferrer">Amaru Ríos</a>
+                Creado por <a href="https://github.com/Am4rv" target="_blank" rel="noopener noreferrer">Amaru Ríos</a>
             </p>
         </footer>
     );
